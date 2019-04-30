461c528a8b853adf2c10021d4fce88979c5a1f65
Jeremy-Q <951339214@qq.com>
2019-04-30 11:41:24 +0800

    整合路由及首页

--- src/App.vue
+++ src/App.vue
@@ -38,9 +38,11 @@
                 <div id="menu2" class="nav-box menuhd">
                     <ul>
                         <li class="index">
-                            <a href="#" class="">
+                            <!-- <a href="#" class=""> -->
+                              <router-link to="/index" >
                                 <span class="out" style="top: 0px;">首页</span>
-                            </a>
+                                </router-link>
+                            <!-- </a> -->
                         </li>
                         <li class="news">
                             <a href="#" class="">
@@ -80,6 +82,8 @@
             </div>
         </div>
     </div>
+    
+    <router-view></router-view>
 
 <!-- 底部区域-->
      <div class="footer">
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+
+
+Vue.use(VueRouter)
+
+import index from './components/index.vue'
+
+const routes= [
+  {path: '/index',
+  component:index 
+  }
+
+]
+
+// 实例化路由对象
+const router = new VueRouter({
+  routes
+})
+
 
 import'./assets/statics/site/css/style.css'
-
 
 
 
@@ -11,8 +30,8 @@ Vue.config.productionTip = false
 
 
 
-
-
+// 挂载到vue实例上
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
