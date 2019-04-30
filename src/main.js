67fed8cfe76f100059d8df3fd985bb151a59e1dd
Jeremy-Q <951339214@qq.com>
2019-04-30 15:51:44 +0800

    首页数据渲染完毕

--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 
+Vue.use(ElementUI);
 Vue.use(VueRouter)
 
 import index from './components/index.vue'
 
 const routes= [
-  {path: '/index',
-  component:index 
-  }
+  {path: '/index',component:index ,},
+  {path : '/' , redirect : '/index'} //访问'/'默认跳转首页
 
 ]
 
