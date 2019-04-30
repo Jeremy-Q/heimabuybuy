485c0220218a2d5e23d334f2a5a45e6b84fec159
Jeremy-Q <951339214@qq.com>
2019-04-30 10:49:17 +0800

    '头尾整合'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import'./assets/statics/site/css/style.css'
+
+
+
+
+
 Vue.config.productionTip = false
+
+
+
+
 
 new Vue({
   render: h => h(App),
