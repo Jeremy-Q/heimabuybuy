3ff372f7caeb216c55777fcb835fdb47b416a66f
Jeremy-Q <951339214@qq.com>
2019-05-02 22:33:20 +0800

    详情页获取和数据渲染

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-
 Vue.use(ElementUI);
-Vue.use(VueRouter)
 
+
+// 首页组件
 import index from './components/index.vue'
+// 详情组件
 import detail from './components/detail.vue'
 
 const routes= [
   {path : '/' , redirect : '/index'}, //访问'/'默认跳转首页
   {path: '/index',component:index },
   {path: '/detail/:id',component:detail },
-
-  
 
 
 ]
@@ -28,9 +27,8 @@ const router = new VueRouter({
 })
 
 
+// 导入全局样式
 import'./assets/statics/site/css/style.css'
-
-
 
 
 Vue.config.productionTip = false
